8ee8bc2e0d8a7a59ccd585222d673a330883c635
Derek Covey <derek.covey@gmail.com>
2018-07-03 14:32:02 -0700

    Create routes to components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/medium.css';
 import { Provider } from 'react-redux';
+
+import { Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,7 +14,11 @@ import { store, history } from './redux/store';
 
 ReactDOM.render((
     <Provider store={store}>
-        <App />
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route path="/" component={App} />
+            </Switch>
+        </ConnectedRouter>
     </Provider>
 ), document.getElementById('root'));
 registerServiceWorker();
